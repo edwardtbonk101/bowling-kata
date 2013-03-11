e4c452e19c4687da873e1c2f7df6c5c9cd71de77
Eddie Bonk <eddie@heysparkbox.com>
2013-03-11 15:52:54 -0400

    Fixed logic issue when spare is rolled in the 10th frame, made code more expressive, added tests

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,44 +1,55 @@
 def score(roll)
 	sum = 0
 	i = 0
-	while i < roll.count
-		if strike?(roll[i])
-			if (roll.length - i) <= 3
-				sum += 10
-				i+=1
-			else
-				sum += strike(roll[i+1], roll[i+2])
-				i += 1
-			end
-		elsif((roll.length - i) > 1 && spare?(roll[i] ,roll[i+1])) 
-			sum += spare(roll[i+2])
+	while i < roll.length
+		if strike?(roll[i]) && tenth_frame?(roll.length - i)
+			sum += 10
+			i+=1
+		elsif strike?(roll[i])
+			sum += strike_score(roll[i+1], roll[i+2])
+			i += 1
+		elsif more_than_one_frame?(roll.length - i) && spare?(roll[i] ,roll[i+1]) && tenth_frame?(roll.length - i)
+			sum += roll[i]
+			i+=1
+		elsif more_than_one_frame?(roll.length - i) && spare?(roll[i] ,roll[i+1])
+			sum += spare_score(roll[i+2])
+			i += 2
+		elsif more_than_one_frame?(roll.length - i)
+			sum += roll[i] + roll[i+1]
 			i += 2
 		else
-			if roll.count - i > 1
-				sum += roll[i] + roll[i+1]
-				i += 2
-			elsif roll.count - i == 1
-				sum += roll[i]
-				i+=1
-			end
+			sum += roll[i]
+			i+=1
 		end
 	end
 	return sum
+end
+
+def last_frame?(frames_left)
+	frames_left == 1
+end
+
+def more_than_one_frame?(frames_left)
+	frames_left > 1
+end
+
+def tenth_frame?(current_frame)
+	current_frame <= 3
 end
 
 def strike?(current_roll)
 	current_roll == 10
 end
 
-def spare?(roll1, roll2)
-	roll1 + roll2 == 10
+def spare?(current_roll, next_roll)
+	current_roll + next_roll == 10
 end
 
-def spare(spare_roll)
+def spare_score(spare_roll)
 	spare_score = 10 + spare_roll
 end
 
-def strike(next_roll1, next_roll2)
+def strike_score(next_roll1, next_roll2)
 	strike_score = 10 + next_roll1 + next_roll2
 end
 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -17,29 +17,56 @@ describe 'score' do
 	it 'should return 300 when all rolls are a strike' do
 		score([10,10,10,10,10,10,10,10,10,10,10,10]).should == 300
 	end
-	it 'should return 100' do 
+	it 'should return 100 when random rolls are made with a turkey in the 10th' do 
 		score([1,1,8,1,9,1,5,5,1,1,0,8,9,1,0,5,4,4,10,10,10]).should == 100
 	end
-	it 'should return 73' do 
+	it 'should return 73 with random assortment of spares are rolled' do 
 		score([1,1,8,1,9,1,5,5,1,1,0,8,9,1,0,5,4,4,1,2]).should == 73
 	end
-	it 'should return 36' do 
+	it 'should return 36 when 2 spares are rolled consecutively' do 
 		score([9,1,5,5,5,1]).should == 36
 	end
-	it 'should return ' do
+	it 'should return 78 when a random assortment of spares and strikes are rolled' do
 		score([1,1,8,1,10,5,5,1,1,0,8,9,1,0,5,4,4,1,2]).should == 78
 	end
-end
-
-describe 'spare' do 
-	it 'should equal 10 plus the score of the next roll' do 
-		spare(9) == 19
+	it 'should return 38 when a spare is rolled in the 10th frame' do 
+		score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,10,1,9]).should == 38
+	end
+	it 'should return 75 when a strikes and spares are rolled' do 
+		score([1,1,1,1,10,9,1,10,1,1,1,1,1,1,1,9,1,1]).should == 75
+	end
+	it 'should return 164 when all strikes and spares are rolled except for the 10th' do 
+		score([1,9,1,9,10,9,1,10,10,1,9,1,9,1,9,9,1,1]).should == 164
 	end
 end
 
-describe 'strike' do 
+describe 'more_than_one_frame?' do
+	it 'should return true if there is more than one frame left' do 
+		more_than_one_frame?(2).should == true
+	end
+	it 'should return false if there is less than one frame left' do 
+		more_than_one_frame?(1).should == false
+	end
+end
+
+describe 'tenth_frame?' do 
+	it 'should return true if the number of frames is less than or equal to 3' do 
+		tenth_frame?(2).should == true
+	end
+	it 'should return false if the number of frames left is greater than 3' do 
+		tenth_frame?(4).should == false
+	end
+end
+
+describe 'spare_score' do 
+	it 'should equal 10 plus the score of the next roll' do 
+		spare_score(9) == 19
+	end
+end
+
+describe 'strike_score' do 
 	it 'should equal 10 plus the score of the next 2 rolls' do 
-		strike(2,2) == 14
+		strike_score(2,2) == 14
 	end
 end
 
