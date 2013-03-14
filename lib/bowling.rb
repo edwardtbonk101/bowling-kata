8bda7dad323f29d1dffe6f583bf93f82f3718860
Eddie Bonk <eddie@heysparkbox.com>
2013-03-14 13:40:18 -0400

    Refactored Everything

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -2,36 +2,28 @@ class Bowling
 	def score(roll)
 		sum = 0
 		i = 0
-		while i < roll.length
-
-			a = roll.length-i
-			b = roll[i+1]
-			b2 = roll[i+2]
-			c = roll[i]
-
-			if ninth_frame_strike?(c, a) && no_extra_frames?((a - 1), b, b2)
-				sum += strike_score(b, b2)
+		
+		(1..9).each do
+		a = roll[i]
+		b = roll[i+1]
+		c = roll[i+2]
+			if strike?(a)
+				sum += strike_score(b, c)
 				i+=1
-			elsif strike?(c) && tenth_frame?(a)
-				sum += 10
-				i+=1
-			elsif strike?(c)
-				sum += strike_score(b, b2)
-				i+=1
-			elsif more_than_one_frame?(a) && spare_in_tenth?(c ,b, a)
-				sum += c
-				i+=1
-			elsif more_than_one_frame?(a) && spare?(c ,b)
-				sum += spare_score(b2)
-				i+=2
-			elsif more_than_one_frame?(a)
-				sum += c + b
+			elsif spare?(a, b)
+				sum += spare_score(c)
 				i+=2
 			else
-				sum += c
-				i+=1
+				sum += a + b
+				i+=2
 			end
 		end
+
+		while i < roll.length
+			sum += roll[i]
+			i+=1
+		end
+		
 		return sum
 	end
 
@@ -47,28 +39,8 @@ class Bowling
 		current_roll + next_roll == 10
 	end
 
-	def spare_in_tenth?(current_roll, next_roll, rolls_left)
-		current_roll + next_roll == 10 && rolls_left <= 3
-	end
-
 	def strike?(current_roll)
 		current_roll == 10
-	end
-
-	def more_than_one_frame?(rolls_left)
-		rolls_left > 1 
-	end
-	
-	def tenth_frame?(current_frame)
-		current_frame <= 3
-	end
-
-	def ninth_frame_strike?(current_roll, rolls_left)
-		current_roll == 10 && rolls_left <=3
-	end
-	
-	def no_extra_frames?(rolls_left, next_roll1, next_roll2)
-		rolls_left == 2 && next_roll1 + next_roll2 < 10
 	end
 end
 
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe 'score' do 
 	it 'should accept rolls' do 
 		bowling = Bowling.new
-		bowling.score([2]).should == 2
+		bowling.score(20.times.map{ 0 }).should == 0
 	end
 
 	it 'should return 20 when all ones are rolled' do
@@ -17,9 +17,9 @@ describe 'score' do
 		bowling.score([10,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).should == 44
 	end
 
-	it 'should return 27 when a spare and 8 and 1 is rolled' do 
+	it 'should return 42 when a spare and 8 and 1 is rolled' do 
 		bowling = Bowling.new
-		bowling.score([9,1,8,1]).should == 27
+		bowling.score([9,1,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).should == 42
 	end
 
 	it 'should return 300 when all rolls are a strike' do
@@ -37,9 +37,9 @@ describe 'score' do
 		bowling.score([1,1,8,1,9,1,5,5,1,1,0,8,9,1,0,5,4,4,1,2]).should == 73
 	end
 
-	it 'should return 36 when 2 spares are rolled consecutively' do 
+	it 'should return 55 when 2 spares are rolled consecutively' do 
 		bowling = Bowling.new
-		bowling.score([9,1,5,5,5,1]).should == 36
+		bowling.score([9,1,5,5,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]).should == 50
 	end
 
 	it 'should return 78 when a random assortment of spares and strikes are rolled' do
@@ -72,6 +72,16 @@ describe 'score' do
 		bowling.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,10,1,1]).should == 30
 	end
 
+	it 'should return 245 when there is 9 strikes and 2 ones' do 
+		bowling = Bowling.new
+		bowling.score([10,10,10,10,10,10,10,10,10,1,1]).should == 245
+	end
+
+	it 'should return 245 when there is 9 strikes and 2 ones' do 
+		bowling = Bowling.new
+		bowling.score([10,10,10,10,10,10,10,10,10,10,1,1]).should == 273
+	end
+
 	it 'should return 29 when there is a strike in the 9th and only 1s in the 10th' do 
 		bowling = Bowling.new
 		bowling.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,1,1,1]).should == 29
@@ -93,7 +103,6 @@ describe 'strike_score' do
 	end
 end
 
-
 describe 'spare?' do 
 	it 'should return true when the sum of 2 frame rolls equals 10' do
 		bowling = Bowling.new
@@ -111,45 +120,5 @@ describe 'strike?' do
 		bowling = Bowling.new
 		bowling.strike?(10).should == true
 	end
-
-	it 'should return false when a 9 is rolled' do 
-		bowling = Bowling.new
-		bowling.strike?(9).should == false
-	end
 end
-
-describe 'more_than_one_frame?' do
-	it 'should return true if there is more than one frame left' do 
-		bowling = Bowling.new
-		bowling.more_than_one_frame?(2).should == true
-	end
-
-	it 'should return false if there is less than one frame left' do 
-		bowling = Bowling.new
-		bowling.more_than_one_frame?(1).should == false
-	end
-end
-
-describe 'tenth_frame?' do 
-	it 'should return true if the number of frames is less than or equal to 3' do 
-		bowling = Bowling.new
-		bowling.tenth_frame?(2).should == true
-	end
-
-	it 'should return false if the number of frames left is greater than 3' do 
-		bowling = Bowling.new
-		bowling.tenth_frame?(4).should == false
-	end
-end
-
-describe 'no_extra_frames?' do 
-	it 'should return true when 2, 1, and 8 is entered' do 
-		bowling = Bowling.new
-		bowling.no_extra_frames?(2,1,8).should == true
-	end
-
-	it 'should return false when 1,1, and 9 is entered'	do 
-		bowling = Bowling.new 
-		bowling.no_extra_frames?(1,1,9).should == false 
-	end
-end
+	
